888a2e9b6a75841e40c0d016037d70ea8ef62703
Arc676/Alessandro Vinciguerra <alesvinciguerra@gmail.com>
2017-09-21 22:00:08 +0800

    Added localization
    C++ backend loads localization for system locale if available

--- main.cpp
+++ main.cpp
@@ -1,15 +1,23 @@
 #include <QGuiApplication>
 #include <QQmlApplicationEngine>
+#include <QTranslator>
 
 int main(int argc, char *argv[])
 {
     QCoreApplication::setAttribute(Qt::AA_EnableHighDpiScaling);
     QGuiApplication app(argc, argv);
 
-    QQmlApplicationEngine engine;
-    engine.load(QUrl(QLatin1String("qrc:/main.qml")));
-    if (engine.rootObjects().isEmpty())
-        return -1;
+	QTranslator *translator = new QTranslator();
+	if (translator->load("qml_" + QLocale::system().name())) {
+		app.installTranslator(translator);
+	}
 
-    return app.exec();
+	QQmlApplicationEngine engine;
+	engine.load(QUrl(QLatin1String("qrc:/main.qml")));
+	if (engine.rootObjects().isEmpty())
+		return -1;
+
+	int ret = app.exec();
+	delete translator;
+	return ret;
 }
